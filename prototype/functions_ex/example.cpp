5d507d256ac494d5250c198c95138a1fed7a41ea
Alec Asatoorian <alec.asatoorian@embodied.me>
2020-01-26 19:03:24 -0800

    adding prototype examples

---
+++ prototype/functions_ex/example.cpp
@@ -0,0 +1,41 @@
+#include <stdio.h> 
+#include <sys/types.h> 
+#include <unistd.h> 
+#include <iostream>
+#include <sys/wait.h>
+
+using namespace std;
+
+int main(){
+	
+	char *args[] = {"./exec", NULL};
+	int status;
+
+	pid_t pid, wpid;
+	
+	pid = fork();
+
+	if(pid == -1){
+		perror("forking");
+		exit(EXIT_FAILURE);
+	}
+	
+	if(pid == 0){
+		execvp(args[0],args);
+	}
+
+	wpid = waitpid(pid, &status, 0);
+	if(WIFEXITED(status)){
+
+		int exit_status = WEXITSTATUS(status);
+		printf("Exit Status: %d", exit_status);
+
+	}
+	else{
+
+		perror("Error: ASA");
+	}
+
+	return 0;
+}
+
---
+++ prototype/functions_ex/exec.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+	cout << "EXECVP: Executed" << endl;
+
+	return 0;
+}
