558a5ccb0b1c35fdb6f374c183fb0fd87d8c337b
asacela <alec.asatoorian@embodied.me>
2020-02-13 06:04:09 -0800

    adding various parser unit tests

--- unit_tests/Parser_Test.hpp
+++ unit_tests/Parser_Test.hpp
@@ -6,6 +6,8 @@
 #include "../header/Parser.hpp"
 #include<vector>
 
+using namespace testing;
+
 
 TEST(SquashTest, SquashSmallInput){
 
@@ -24,9 +26,115 @@ TEST(SquashTest, SquashSmallInput){
     vector<Base*> baseList;
     baseList.push_back(squashed1);
     Base* squashed2 = test->getSquashed(baseList);
-    
+
     /* compares resulting objects' strings with stringify */
     EXPECT_EQ(squashed1->stringify(), squashed2->stringify());
 }
+TEST(SquashTest, SquashExecuteCommandBadArg){
+
+
+	/* objects for testing squash, stringify */
+    Parser* test = new Parser("ls -j");
+    Base* cmd1 = new Command({"ls", "-j"});
+
+
+    /* set up second base* for comparison */
+    vector<Base*> baseList;
+    baseList.push_back(cmd1);
+    Base* squashed2 = test->getSquashed(baseList);
+
+    /* compares resulting objects' strings with stringify */
+    EXPECT_TRUE(squashed2->execute() == true);
+}
+TEST(SquashTest, SquashExecuteInvalidCommand){
+
+
+	/* objects for testing squash, stringify */
+    Parser* test = new Parser("gert12 floofy && git status");
+    Base* cmd1 = new Command({"gert12", "floofy"});
+    Base* cmd2 = new Command({"git", "status"});
+
+
+    /* set up first base* for comparison */
+    Base* squashed1 = new And(cmd1, cmd2);
+
+
+    /* set up second base* for comparison */
+    vector<Base*> baseList;
+    baseList.push_back(squashed1);
+    Base* squashed2 = test->getSquashed(baseList);
+
+    /* compares resulting objects' strings with stringify */
+    EXPECT_TRUE(squashed2->execute() == true);
+}
+TEST(SquashTest, SquashExecuteAndConnector){
+
+
+	/* objects for testing squash, stringify */
+    Parser* test = new Parser("ls -a && git status");
+    Base* cmd1 = new Command({"ls", "-j"});
+    Base* cmd2 = new Command({"git", "status"});
+
+
+    /* set up first base* for comparison */
+    Base* squashed1 = new And(cmd1, cmd2);
+
+
+    /* set up second base* for comparison */
+    vector<Base*> baseList;
+    baseList.push_back(squashed1);
+    Base* squashed2 = test->getSquashed(baseList);
+
+    /* compares resulting objects' strings with stringify */
+    EXPECT_TRUE(squashed2->execute() == true);
+}
+TEST(SquashTest, SquashExecuteOrConnector){
+
+
+	/* objects for testing squash, stringify */
+    Parser* test = new Parser("ls -a || git status");
+    Base* cmd1 = new Command({"ls", "-j"});
+    Base* cmd2 = new Command({"git", "status"});
+
+
+    /* set up first base* for comparison */
+    Base* squashed1 = new And(cmd1, cmd2);
+
+
+    /* set up second base* for comparison */
+    vector<Base*> baseList;
+    baseList.push_back(squashed1);
+    Base* squashed2 = test->getSquashed(baseList);
+
+    /* compares resulting objects' strings with stringify */
+    EXPECT_TRUE(squashed2->execute() == true);
+}
+TEST(SquashTest, SquashExecuteSemicolonConnector){
+
+
+	/* objects for testing squash, stringify */
+    Parser* test = new Parser("ls -a; git status");
+    Base* cmd1 = new Command({"ls", "-j"});
+    Base* cmd2 = new Command({"git", "status"});
+
+
+    /* set up first base* for comparison */
+    Base* squashed1 = new And(cmd1, cmd2);
+
+
+    /* set up second base* for comparison */
+    vector<Base*> baseList;
+    baseList.push_back(squashed1);
+    Base* squashed2 = test->getSquashed(baseList);
+
+    /* compares resulting objects' strings with stringify */
+    EXPECT_TRUE(squashed2->execute() == true);
+}
+
+
+
+
+
+
 
 #endif
