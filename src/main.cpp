64adf331d40a11174bdef788aaa691a98ab5b166
asacela <alec.asatoorian@embodied.me>
2020-02-13 06:03:04 -0800

    adding prac.cpp logic to main.cpp for continuous shell

--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,22 @@ using namespace std;
 
 int main(){
 
-	
+	int num = 1;
+	for(int i = 0; i < num; ++i){
+		cout << "$ ";
+		string cmdLine;
+		getline(cin,cmdLine);
+
+		Parser* parserObj = new Parser(cmdLine);
+		parserObj->parse();
+		Base* compObj = parserObj->parse();
+		if(compObj != nullptr){
+			cout << endl << "--DISPLAY--" << endl;
+			cout << compObj->stringify();
+			cout << endl << "--EXECUTE--" << endl;
+			compObj->execute();
+		}
+
+	}
 
 }
