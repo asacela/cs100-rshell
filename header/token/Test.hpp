bc753f000e38af4a95a377d85028268801ab4e82
asacela <alecasatoorian77@gmail.com>
2020-02-28 00:03:45 -0800

    Jgomex89/test prototype (#26)
    * adding test
    
    * adding fixed test execute class
    
    Co-authored-by: JGomez89 <47131705+JGomez89@users.noreply.github.com>

--- header/token/Test.hpp
+++ header/token/Test.hpp
@@ -4,6 +4,9 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
 #include "Command.hpp"
 
 using namespace std;
@@ -15,43 +18,109 @@ public:
 	/* Constructors */
 	Test(vector<string> parsed_) : Command(parsed_) {
 
-    if(parsed.size() != 0){
-      if(parsed.front() == "[" && parsed.back() == "]"){
-        vector<string>::iterator it = parsed.begin();
-        parsed.erase(it);
-        parsed.pop_back();
-      }
+        if(parsed.size() != 0){
+            if(parsed.front() == "[" && parsed.back() == "]"){
+                vector<string>::iterator it = parsed.begin();
+                parsed.erase(it);
+                parsed.pop_back();
+            }
 
-      if(parsed.front() != "test"){
-        vector<string>::iterator it = parsed.begin();
-        parsed.insert(it,"test");
-      }
+            if(parsed.front() != "test"){
+                vector<string>::iterator it = parsed.begin();
+                parsed.insert(it,"test");
+            }
+          
+            if(parsed.size() >= 2){
+ 
+                if(parsed.at(1).front() != '-'){
+                vector<string>::iterator it = parsed.begin();
+                parsed.insert(it+1,"-e");
+                }
+            }
+        }
 
-      if(parsed.at(1).front() != '-'){
-        vector<string>::iterator it = parsed.begin();
-        parsed.insert(it+1,"-e");
-      }
-    }
 
+        to_cstring();
 
-    to_cstring();
 	}
 
 	/* Pure Virtual Functions */
 	virtual bool execute(){
-    bool test = Command::execute();
-    if(test){
-      cout << "(TRUE)" << "\n";
-    }
-    else{
-      cout << "(FALSE)" << "\n";
-    }
-    return test;
+        struct stat sb;
+
+		if(parsed.size() == 3){
+
+
+
+			if(stat(argList[2], &sb) < 0){
+
+                perror("rshell: error in running stat()");
+                cout << "(FALSE)" << "\n";
+
+                return true;
+            }
+
+			bool result = false;
+
+			string flag = parsed.at(1);
+			if(flag == "-e"){
+				result = exists(sb.st_mode);
+			}
+			else if(flag == "-d"){
+				result = isDir(sb.st_mode);
+			}
+			else if(flag == "-f"){
+				result = isFile(sb.st_mode);
+			}
+			else{
+				// Unknown flag
+			}
+
+	    // bool test = Command::execute();
+	    if(result){
+
+	      cout << "(TRUE)" << "\n";
+	    }
+	    else{
+
+	      cout << "(FALSE)" << "\n";
+	    }
+
+		return true;
+		}
+
+    return false;
 	}
 
+
 private:
-	vector<string> parsed;
-  const char** argList;
+	bool isDir(mode_t m){
+
+        if(S_ISDIR(m) > 0){
+            return true;
+        }
+
+		return false;
+	}
+
+	bool isFile(mode_t m){
+
+		if(S_ISREG(m) > 0){
+            return true;
+        }
+
+        return false;
+	}
+
+	bool exists(mode_t m){
+
+        if(S_ISDIR(m) > 0 || S_ISREG(m) > 0){
+            return true;
+        }
+
+        return false;
+	}
+
 };
 
 #endif
