8dd4a799896403215214f673f354d160dd0a4136
asacela <alec.asatoorian@embodied.me>
2020-02-28 13:20:06 -0800

    adding new integration tests

---
+++ integration_tests/test_literal_tests.sh
@@ -0,0 +1,15 @@
+INPUTS=("git pull" "echo hello" "ls -a" "git status" "ls -j")
+
+for input in "${INPUTS[@]}"
+do
+	echo "---------Single Command Test ${@}:----------"
+	echo " ${input}"
+	${input}
+
+	sleep 1
+
+	
+	echo "-----------------Completed------------------"
+	echo "\n\n\n\n"
+
+done
