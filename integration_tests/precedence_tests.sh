6b1d0b94309eb1cd3af26a093a52aa3f6b5e5246
asacela <alecasatoorian77@gmail.com>
2020-02-28 23:23:34 -0800

    adding cases and .sh for integration testing

--- integration_tests/precedence_tests.sh
+++ integration_tests/precedence_tests.sh
@@ -1,15 +1,2 @@
-INPUTS=("(echo hello && (echo hell nah || (git status && ls -a)))" "(echo making && (ls -j || git status))" "((echo hello || ls -a) && (ls -j))")
-
-for input in "${INPUTS[@]}"
-do
-	echo "---------Precedence Command Test ${@}:----------"
-	echo " ${input}"
-	${input}
-
-	sleep 1
-
-	
-	echo "-----------------Completed------------------"
-	echo "\n\n\n\n"
-
-done
+#!/bin/sh
+../rshell < precedence_cases
