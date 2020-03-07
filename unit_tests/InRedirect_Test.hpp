29b9de2ec92bd61863d10092fa3c451beac2f5cd
asacela <alec.asatoorian@embodied.me>
2020-03-06 23:49:50 -0800

    adding inredirect_test

---
+++ unit_tests/InRedirect_Test.hpp
@@ -0,0 +1,25 @@
+#ifndef __AND_TEST_HPP__
+#define __AND_TEST_HPP__
+
+#include "gtest/gtest.h"
+
+#include "../header/token/Connectors/And.hpp"
+#include "../header/token/Connectors/Command.hpp"
+#include "../header/token/Connectors/InRedirect.hpp"
+
+
+TEST(InRedirect, BasicTest) {
+    vector<string> vct1 = {"echo",  "hello"};
+    vector<string> vct2 = {"/text_files/new.txt"};
+
+
+    Base* cmd1 = new Command(vct1);
+    Base* cmd2 = new Command(vct2);
+
+
+    Base* test = new InRedirect(cmd1, cmd2);
+    EXPECT_EQ(test->stringify(), "ls -a < git status");
+}
+
+
+#endif
