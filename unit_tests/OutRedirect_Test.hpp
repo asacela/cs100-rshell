903da3ed0942a556ba766604cda33d9e83a9a200
jgomez89 <justin.gomez3789@gmail.com>
2020-03-09 10:42:46 -0700

    small changes to tests

--- unit_tests/OutRedirect_Test.hpp
+++ unit_tests/OutRedirect_Test.hpp
@@ -19,21 +19,32 @@ TEST(OutRedirectOverwrite, BasicTest) {
 
     Base* cmd1 = new Command(vct1);
     Base* cmd2 = new Command(vct2);
-
-
     Base* test = new OutRedirect(">",cmd1, cmd2);
+
+
+    /* Check textFilefile before overwriting */
+    // Write into textFile
+    std::ofstream fout;
+    fout.open(textFile.c_str(), std::ofstream::out | std::ofstream::trunc);
+    fout << "OVERWRITE THIS TEXT\n";
+    fout.close();
+    // Check textFile
+    std::ifstream fin;
+    fin.open(textFile.c_str());
+    std::string contentBefore( (std::istreambuf_iterator<char>(fin)) ,(std::istreambuf_iterator<char>()));
+    fin.close();
+    EXPECT_EQ(contentBefore,"OVERWRITE THIS TEXT\n");
+
+    /* Overwrite textFile */
     EXPECT_TRUE(test->execute());
     EXPECT_EQ(test->stringify(), "echo HELLO 123 > " + textFile);
 
-    std::ifstream fin;
+    /* Check textFilefile after overwriting */
     fin.open(textFile.c_str());
-    std::string content( (std::istreambuf_iterator<char>(fin)) ,(std::istreambuf_iterator<char>()));
+    std::string contentAfter( (std::istreambuf_iterator<char>(fin)) ,(std::istreambuf_iterator<char>()));
     fin.close();
-    EXPECT_EQ(content,"HELLO 123\n");
+    EXPECT_EQ(contentAfter,"HELLO 123\n");
 
-    std::ofstream fout;
-    fout.open(textFile.c_str(), std::ofstream::out | std::ofstream::trunc);
-    fout.close();
 }
 
 
@@ -51,21 +62,29 @@ TEST(OutRedirectAppend, BasicTest) {
     Base* test = new OutRedirect(">>",cmd1, cmd2);
 
 
+    /* Check textFilefile before appending */
+    // Write into textFile
     std::ofstream fout;
     fout.open(textFile.c_str(), std::ofstream::out | std::ofstream::trunc);
     fout << "APPEND TO THIS TEXT\n";
     fout.close();
+    // Check textFile
+    std::ifstream fin;
+    fin.open(textFile.c_str());
+    std::string contentBefore( (std::istreambuf_iterator<char>(fin)) ,(std::istreambuf_iterator<char>()));
+    fin.close();
+    EXPECT_EQ(contentBefore,"APPEND TO THIS TEXT\n");
 
 
+    /* Append to textFile */
     EXPECT_TRUE(test->execute());
     EXPECT_EQ(test->stringify(), "echo hello >> " + textFile);
 
-
-    std::ifstream fin;
+    /* Check textFile after appending */
     fin.open(textFile.c_str());
-    std::string content( (std::istreambuf_iterator<char>(fin)) ,(std::istreambuf_iterator<char>()));
+    std::string contentAfter( (std::istreambuf_iterator<char>(fin)) ,(std::istreambuf_iterator<char>()));
     fin.close();
-    EXPECT_EQ(content,"APPEND TO THIS TEXT\nhello\n");
+    EXPECT_EQ(contentAfter,"APPEND TO THIS TEXT\nhello\n");
 
 
 }
