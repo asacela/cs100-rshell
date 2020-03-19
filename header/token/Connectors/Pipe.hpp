0a0bff32a10ce06808c02b32c3c2124874d9584b
asacela <alec.asatoorian@embodied.me>
2020-03-19 14:01:36 -0700

    adding pipe

--- header/token/Connectors/Pipe.hpp
+++ header/token/Connectors/Pipe.hpp
@@ -24,64 +24,30 @@ public:
 	virtual bool execute(){
 
 		int pipefd[2];
-		int pid1;
-		int pid2;
 
-		int status;
+	    if(pipe(pipefd) == -1) {
 
-		pipe(pipefd);
+	        perror("pipe");
+	        return false;
+	    }
 
-		// Process 1
-		pid1 = fork();
-		if(pid1 < 0){
+	    dup2(pipefd[0], 0);
+	    if(!lhs->execute()) {
 
-			perror("rshell: error forking child process");
-			return false;
-		}
-
-		else if(pid1 == 0)
-		{
-
-			dup2(pipefd[0], 0);
-			close(pipefd[1]);
-			lhs->execute();
-		}
-
-		else{
-
-			pid_t check = wait(&status);
-			while(check != pid1);
-		}
+	        return false;
+	    }
+	    close(pipefd[1]);
 
 
-		// Process 2
-		pid2 = fork();
-		if(pid2 < 0){
-			
-			perror("rshell: error forking child process");
-			return false;
-		}
+	    dup2(pipefd[1], 1);
+	    if(!rhs->execute()) {
 
-		else if(pid2 == 0)
-		{
+	        return false;
+    	}
+    	close(pipefd[0]);
 
-			dup2(pipefd[1], 1);
-
-			close(pipefd[0]);
-
-			rhs->execute();
-
-			return true;
-		}
-
-		else
-		{
-
-			pid_t check = wait(&status);
-			while(check != pid2);
-		}
-
-		return false;
+    	return true;
+		
 	}
 
 	virtual string stringify(){
